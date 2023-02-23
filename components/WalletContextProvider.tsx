1e94fabe230586f1a67b8b0722abd9574a2a3c77
Shubham Prasad <prasadshubham821@gmail.com>
2023-02-23 23:44:23 +0530

    Initial commit

---
+++ components/WalletContextProvider.tsx
@@ -0,0 +1,23 @@
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { FC, ReactNode, useMemo } from "react";
+
+const WalletContextProvider: FC<{ children: ReactNode }> = ({ children }) => {
+  const url = useMemo(() => clusterApiUrl("devnet"), []);
+  const phantom = new PhantomWalletAdapter();
+
+  return (
+    <ConnectionProvider endpoint={url}>
+      <WalletProvider wallets={[phantom]}>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
